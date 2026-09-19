9745d24069b00d2713baab34af9ff6529b41a693
demoskratos14 <demoskratos14@gmail.com>
2026-09-19 21:33:46 +0200

    Add files via upload

---
+++ app/src/main/java/com/aventure/desdice/model/Story.kt
@@ -0,0 +1,9 @@
+package com.aventure.desdice.model
+
+data class Story(
+    val slug: String,
+    val title: String,
+    val subtitle: String,
+    val bgImageB64: String,
+    val isCustom: Boolean
+)
